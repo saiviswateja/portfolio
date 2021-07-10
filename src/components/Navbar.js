e29363cdfdb854b65edafccfc38d5154e56a1aa6
saiviswatejasreerama <saiviswatejasreerama@outlook.com>
2021-07-10 17:38:41 +0530

    updated resume link and job descriptions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,10 @@ import {useHistory} from 'react-router-dom';
 
 function Navbar() {
     const history = useHistory();
-    const downloadFile = ()=>{
-        
-    }
     return (
         <div>
             <div class="d-flex flex-row-reverse bd-highlight navcontainer">
-                <div class="p-2 bd-highlight"><span className="navtext"><a href="https://drive.google.com/file/d/137mNia5Qp_aQqa-TBjmhsKmuAcfqvu8d/view" target="_blank">Resume</a></span></div>
+                <div class="p-2 bd-highlight"><span className="navtext"><a href="https://drive.google.com/file/d/1TOuyqgroAgAf7mLAG_a1gEw4L_ZRn6pi/view?usp=sharing" target="_blank">Resume</a></span></div>
                 <div class="p-2 bd-highlight"><span className="navtext" onClick={()=>{
                     history.push("/participations");
                 }}>Participations</span></div>
