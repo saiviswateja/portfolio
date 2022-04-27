5e9461e99a5fbb447ad6720a397eb82d2f0156ac
saiviswateja <saiviswateja.teja@gmail.com>
2022-04-27 19:30:32 +0530

    changed the resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     return (
         <div>
             <div class="d-flex flex-row-reverse bd-highlight navcontainer">
-                <div class="p-2 bd-highlight"><span className="navtext"><a href="https://drive.google.com/file/d/1TOuyqgroAgAf7mLAG_a1gEw4L_ZRn6pi/view?usp=sharing" target="_blank">Resume</a></span></div>
+                <div class="p-2 bd-highlight"><span className="navtext"><a href="https://drive.google.com/file/d/1tfyxLwlpAatGEwB1z07kcCXziTsYgXE6/view?usp=sharing" target="_blank">Resume</a></span></div>
                 <div class="p-2 bd-highlight"><span className="navtext" onClick={()=>{
                     history.push("/participations");
                 }}>Participations</span></div>
