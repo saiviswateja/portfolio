ba1d2bfef6de8c157c87398c20a78b053eb1f2f1
saiviswateja <saiviswateja.teja@gmail.com>
2022-04-27 19:36:49 +0530

    just a indentation change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 function Navbar() {
     const history = useHistory();
     return (
         <div>
             <div class="d-flex flex-row-reverse bd-highlight navcontainer">
-                <div class="p-2 bd-highlight"><span className="navtext"><a href="https://drive.google.com/file/d/1tfyxLwlpAatGEwB1z07kcCXziTsYgXE6/view?usp=sharing" target="_blank">Resume</a></span></div>
-                <div class="p-2 bd-highlight"><span className="navtext" onClick={()=>{
+                <div class="p-2 bd-highlight">
+                    <span className="navtext">
+                        <a href="https://drive.google.com/file/d/1tfyxLwlpAatGEwB1z07kcCXziTsYgXE6/view?usp=sharing" target="_blank">Resume</a>
+                    </span>
+                </div>
+                <div class="p-2 bd-highlight"><span className="navtext" onClick={() => {
                     history.push("/participations");
                 }}>Participations</span></div>
             </div>
