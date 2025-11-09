b82200d6a7edf8fd58a2fbf894f8c3f966e8c7f3
saiviswateja <32026497+saiviswateja@users.noreply.github.com>
2025-11-09 15:16:59 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Replace with your repo name, e.g. '/sai-portfolio/'
-const base = '/saiviswateja'
+const base = '/'
 
 export default defineConfig({
   plugins: [react()],
